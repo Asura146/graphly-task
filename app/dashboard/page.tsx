d85d33b55c6737918fc0494ba5fe4b654c7ce361
Asura146 <kyo008g@gmail.com>
2026-02-01 01:36:17 +0900

    ダッシュボードページを新規作成

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboardPage() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>ダッシュボード</h1>
+            <p>ここは認証が必要なページです。</p>
+        </div>
+    );
+}
