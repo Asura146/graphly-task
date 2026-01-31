6ecd62557802d8d76b6cf08006d58334b79d80fc
Asura146 <kyo008g@gmail.com>
2026-02-01 02:46:23 +0900

    ダッシュボードページを新規作成し、古い実装を削除

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+export default function dashboardPage() {
+    return (
+      <>
+
+        <div className="pt-24 px-6 max-w-5xl mx-auto h-1000">
+            <h1 className="text-2xl font-bold mt-4">ダッシュボード</h1>
+        </div>
+      </>
+    );
+}
--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function dashboardPage() {
-    return (
-        <div style={{ padding: "20px" }}>
-            <h1>ダッシュボード</h1>
-            <p>ここは認証が必要なページです。</p>
-        </div>
-    );
-}
