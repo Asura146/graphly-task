e02ae1360808569f3dd701109537cd3a239ed898
Asura146 <kyo008g@gmail.com>
2026-02-09 01:06:31 +0900

    ホームページからユーザー情報とアカウント削除ボタンを削除し、ダッシュボードへのリダイレクトを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import { UserInfo } from "@/components/user-info";
-import { DeleteAccountButton } from "@/components/user-del";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-    return (
-        <main style={{ padding: "20px" }}>
-            <h1>マイアプリへようこそ</h1>
-            {/* ここで呼び出す */}
-            <UserInfo />
-            <DeleteAccountButton />
-        </main>
-    );
+  redirect("/dashboard");
 }
